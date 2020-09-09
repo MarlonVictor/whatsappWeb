6a8f72d41a93d3e4a67a8dfd078004467fdce3ae
MarlonVictor <cmarlonvictor11@gmail.com>
2020-09-09 18:55:12 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <h2>
+            <img src="https://image.flaticon.com/icons/svg/1384/1384055.svg" alt="icon" width="40px"/>
+            WHATSAPP WEB
+        </h2>
+    )
+}
+
+export default App;
